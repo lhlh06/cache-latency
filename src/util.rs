1c0b792178d9d541771a8b6e726df3daab7bd4d5
lhlh06 <whlbx20124@163.com>
2026-02-11 00:08:02 +0800

    add compiler fence in tsc functions

--- src/util.rs
+++ src/util.rs
@@ -4,16 +4,14 @@
 pub fn raw_fenced(clock: &quanta::Clock) -> u64 {
     use std::sync::atomic::{Ordering, compiler_fence};
 
-    compiler_fence(Ordering::SeqCst);
     unsafe {
+        compiler_fence(Ordering::SeqCst);
         core::arch::x86_64::_mm_lfence();
-    }
-    let t = clock.raw();
-    unsafe {
+        let t = clock.raw();
         core::arch::x86_64::_mm_lfence();
+        compiler_fence(Ordering::SeqCst);
+        t
     }
-    compiler_fence(Ordering::SeqCst);
-    t
 }
 
 /// Returns the duration of `Median` overhead of 2x[raw_fenced] and 1 read tsc clock
@@ -35,11 +33,16 @@ pub fn measure_overhead_ns(clock: &quanta::Clock, num_iterations: usize) -> u64 
 #[cfg(all(target_arch = "x86_64", target_feature = "sse2"))]
 #[inline(always)]
 pub fn tsc_start() -> u64 {
-    use std::arch::x86_64::{_mm_lfence, _rdtsc};
+    use std::{
+        arch::x86_64::{_mm_lfence, _rdtsc},
+        sync::atomic::{Ordering, compiler_fence},
+    };
     unsafe {
+        compiler_fence(Ordering::SeqCst);
         _mm_lfence();
         let tsc = _rdtsc();
         _mm_lfence();
+        compiler_fence(Ordering::SeqCst);
 
         tsc
     }
@@ -48,11 +51,16 @@ pub fn tsc_start() -> u64 {
 #[cfg(all(target_arch = "x86_64", target_feature = "sse2"))]
 #[inline(always)]
 pub fn tsc_end() -> u64 {
-    use std::arch::x86_64::{__rdtscp, _mm_lfence};
+    use std::{
+        arch::x86_64::{__rdtscp, _mm_lfence},
+        sync::atomic::{Ordering, compiler_fence},
+    };
     let mut aux = 0;
     unsafe {
+        compiler_fence(Ordering::SeqCst);
         let tsc: u64 = __rdtscp(&mut aux);
         _mm_lfence();
+        compiler_fence(Ordering::SeqCst);
         tsc
     }
 }
