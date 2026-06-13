ec0d4ef369d8ef74b430e9407b45b3dc917ec142
Xu <whlbx20124@163.com>
2026-06-14 07:34:24 +0800

    remove extra `lfence` & add tsc overhead measurement

--- src/util.rs
+++ src/util.rs
@@ -1,36 +1,4 @@
 #[cfg(all(target_arch = "x86_64", target_feature = "sse2"))]
-#[allow(unused)]
-#[inline(always)]
-pub fn raw_fenced(clock: &quanta::Clock) -> u64 {
-    use std::sync::atomic::{Ordering, compiler_fence};
-
-    unsafe {
-        compiler_fence(Ordering::SeqCst);
-        core::arch::x86_64::_mm_lfence();
-        let t = clock.raw();
-        core::arch::x86_64::_mm_lfence();
-        compiler_fence(Ordering::SeqCst);
-        t
-    }
-}
-
-/// Returns the duration of `Median` overhead of 2x[raw_fenced] and 1 read tsc clock
-#[allow(unused)]
-pub fn measure_overhead_ns(clock: &quanta::Clock, num_iterations: usize) -> u64 {
-    let mut overhead = Vec::with_capacity(num_iterations);
-
-    for _ in 0..num_iterations {
-        let start = raw_fenced(clock);
-        let end = raw_fenced(clock);
-        let ns = clock.delta_as_nanos(start, end);
-        overhead.push(ns);
-    }
-
-    overhead.sort_by(|a, b| a.partial_cmp(b).unwrap());
-    overhead[overhead.len() / 2]
-}
-
-#[cfg(all(target_arch = "x86_64", target_feature = "sse2"))]
 #[inline(always)]
 pub fn tsc_start() -> u64 {
     use std::{
@@ -41,7 +9,6 @@ pub fn tsc_start() -> u64 {
         compiler_fence(Ordering::SeqCst);
         _mm_lfence();
         let tsc = _rdtsc();
-        _mm_lfence();
         compiler_fence(Ordering::SeqCst);
 
         tsc
@@ -61,6 +28,29 @@ pub fn tsc_end() -> u64 {
         let tsc: u64 = __rdtscp(&mut aux);
         _mm_lfence();
         compiler_fence(Ordering::SeqCst);
+
         tsc
     }
+}
+
+pub fn measure_tsc_overhead(num_iterations: usize) -> u64 {
+    // better be 500 or larger
+    let num_iterations = num_iterations.max(500);
+
+    let mut overhead = Vec::with_capacity(num_iterations);
+
+    // warmup
+    for _ in 0..100 {
+        let _ = tsc_start();
+        let _ = tsc_end();
+    }
+
+    for _ in 0..num_iterations {
+        let start = tsc_start();
+        let end = tsc_end();
+        overhead.push(end.saturating_sub(start));
+    }
+
+    overhead.sort_unstable();
+    overhead[overhead.len() / 2]
 }
